ee8b42984ba02e04d14e1f8352a8c1474206cd7f
Danilo Recchia <danilo@deltatecnologia.com>
2020-05-05 18:30:42 -0300

    Update api.js
    Reading container variables.

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-  host: 'http://vroom-backend',
-  port: '3000',
+  host: process.env.HOST || 'http://localhost',
+  port: process.env.PORT || '3000',
   maxJobNumber: 1000
 };
